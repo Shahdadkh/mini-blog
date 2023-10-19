ac7e40b13ef313b254b07db6f20d4b8739e41cf5
ShahdadKh <mr.khoshbakht91@gmail.com>
2023-10-19 18:30:15 +0330

    Create Inbox

--- app/panel/edit-aboutus/page.tsx
+++ app/panel/edit-aboutus/page.tsx
@@ -1,8 +1,94 @@
+"use client";
+import { Formik, Form, Field } from "formik";
+import Link from "next/link";
+
+interface typeValue {
+  text: string;
+  fb: string;
+  tw: string;
+  tel: string;
+  ig: string;
+}
+
 const EditAboutUs = () => {
+  const initialValues: typeValue = {
+    text: "",
+    fb: "",
+    tw: "",
+    tel: "",
+    ig: "",
+  };
+
+  const handleSubmit = (value: any) => {
+    console.log(value);
+  };
+
   return (
-    <>
-      <h1>EditAboutUs</h1>
-    </>
+    <div className="w-3/6 mx-auto mt-20">
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form>
+          <div className=" w-11/12 mx-auto my-5">
+            <div className="mb-3">
+              <Field
+                as="textarea"
+                rows="8"
+                name="text"
+                className="border border-transparent block w-full mt-6 py-2 px-3 rounded-xl shadow-custom-shadow outline-none fontcolor1"
+                placeholder="متن خود را وارد کنید"
+              />
+            </div>
+            <div className="grid grid-cols-2  gap-4">
+              <div>
+                <Field
+                  type="text"
+                  name="fb"
+                  className="border border-transparent block w-full  py-2 px-3 rounded-xl shadow-custom-shadow outline-none fontcolor1"
+                  placeholder="لینک فیس بوک"
+                />
+              </div>
+              <div>
+                <Field
+                  type="text"
+                  name="tw"
+                  className="border border-transparent block w-full  py-2 px-3 rounded-xl shadow-custom-shadow outline-none fontcolor1"
+                  placeholder="لینک توییتر"
+                />
+              </div>
+              <div>
+                <Field
+                  type="text"
+                  name="tel"
+                  className="border border-transparent block w-full  py-2 px-3 rounded-xl shadow-custom-shadow outline-none fontcolor1"
+                  placeholder="لینک تلگرام"
+                />
+              </div>
+              <div>
+                <Field
+                  type="text"
+                  name="ig"
+                  className="border border-transparent block w-full  py-2 px-3 rounded-xl shadow-custom-shadow outline-none fontcolor1"
+                  placeholder="لینک اینستاگرام"
+                />
+              </div>
+            </div>
+            <div className="flex justify-end w-full mt-3">
+              <Link
+                href="/about-us"
+                className="bg-white fontcolor1 border border-gray-400 cursor-pointer ml-2 font-medium text-base py-2 px-10 mt-1 rounded-full"
+              >
+                نمایش
+              </Link>
+              <button
+                type="submit"
+                className="bg-gray-500 hover:bg-gray-600 text-white font-medium text-base py-2 px-12 mt-1 rounded-full"
+              >
+                ثبت
+              </button>
+            </div>
+          </div>
+        </Form>
+      </Formik>
+    </div>
   );
 };
 
--- app/panel/inbox/page.tsx
+++ app/panel/inbox/page.tsx
@@ -1,8 +1,119 @@
+"use client";
+import Pagination from "@/components/common/Pagination";
+import Link from "next/link";
+import { useState } from "react";
+
+export const data = [
+  {
+    id: 1,
+    name: "محمد",
+    title:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. ",
+    date: "13:50 - 1402/07/05",
+    text: "متن بسیار زیبایی بود",
+    active: true,
+    answer: "خواهش می‌کنم",
+  },
+  {
+    id: 2,
+    name: "محمد",
+    title: "متن شماره دو",
+    date: "13:00 - 1402/08/05",
+    text: "متن بسیار زیبایی بود",
+    active: false,
+    answer: "",
+  },
+  {
+    id: 3,
+    name: "محمد",
+    title: "متن شماره یک",
+    date: "13:50 - 1402/07/05",
+    text: "متن بسیار زیبایی بود",
+    active: true,
+    answer: "",
+  },
+  {
+    id: 4,
+    name: "محمد",
+    title: "متن شماره یک",
+    date: "13:50 - 1402/07/05",
+    text: "متن بسیار زیبایی بود",
+    active: true,
+    answer: "متشکرم",
+  },
+];
+
 const Inbox = () => {
+  const [files, setFiles] = useState(data);
+  const textLength = 60;
+  //Pagination
+  const [pageSize, setPageSize] = useState(10);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const StartCourse = Number(currentPage) * Number(pageSize);
+  const EndCourse = Number(currentPage) * Number(pageSize) + Number(pageSize);
+
+  const handleAnswer = () => {
+    console.log("answer");
+  };
+
+  const handleDeactive = () => {
+    console.log("Deactive");
+  };
+
   return (
-    <>
-      <h1>Inbox</h1>
-    </>
+    <div className="mb-4">
+      {files
+        .sort((a, b) => b.id - a.id)
+        .slice(StartCourse, EndCourse)
+        .map((file, i) => (
+          <div
+            key={i}
+            className="w-11/12 h-fit py-4 rounded-xl mx-auto mt-5 shadow-custom-shadow"
+          >
+            <div className="flex justify-between mx-4">
+              <Link href={`/post/${file.id}`} className="text-sm font-semibold">
+                {`${file.name} در پست ${
+                  file.title.length > textLength
+                    ? `${file.title.slice(0, textLength)}...`
+                    : file.title
+                }`}
+              </Link>
+              <div className="text-xs font-semibold">{file.date}</div>
+            </div>
+            <div className="text-sm fontcolor1 mx-4">{file.text}</div>
+            <div className="mt-4">
+              <button
+                onClick={() => handleAnswer()}
+                className="bg-gray-500 hover:bg-gray-600 text-white font-normal text-sm py-1.5 px-8 mr-3 ml-1 mt-1 rounded-full"
+              >
+                پاسخ
+              </button>
+              <button
+                onClick={() => handleDeactive()}
+                className="bg-white fontcolor1 border border-gray-400 font-normal text-sm py-1.5 w-24 mt-1 rounded-full"
+              >
+                {file.active ? "غیرفعال" : "فعال"}
+              </button>
+            </div>
+            {file.answer && (
+              <div className="w-[98%] h-fit text-sm rounded-xl mt-2 mx-auto p-3 bg-gray-200">
+                {file.answer}
+              </div>
+            )}
+          </div>
+        ))}
+      {files.length > pageSize && (
+        <div className="my-4">
+          <Pagination
+            totalPageSize={files.length}
+            pageSize={pageSize}
+            setCurrentPage={setCurrentPage}
+            currentPage={currentPage}
+          />
+        </div>
+      )}
+    </div>
   );
 };
 
