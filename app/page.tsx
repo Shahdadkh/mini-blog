6de7742ddf9dc9be6c2c9e5595386c8837c21235
ShahdadKh <mr.khoshbakht91@gmail.com>
2023-10-25 19:08:32 +0330

    set get request for landing and posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,22 @@
 "use client";
 import Link from "next/link";
 import Pagination from "@/components/common/Pagination";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  const textList = [
-    {
-      id: 1,
-      title: "متن شماره یک در راه اژده ها",
-      date: "1402/07/25",
-      text: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.",
-    },
-    {
-      id: 2,
-      title: "متن شماره دو در راه اژده ها",
-      date: "1402/07/26",
-      text: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.",
-    },
-    {
-      id: 3,
-      title: "متن شماره دو در راه اژده ها",
-      date: "1402/07/26",
-      text: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.",
-    },
-  ];
-
   const textLength = 500;
-  const [files, setFiles] = useState(textList);
+  const [files, setFiles] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        setFiles(data);
+      });
+  }, []);
 
   //Pagination
-  const [pageSize, setPageSize] = useState(8);
+  const pageSize = 8;
   const [currentPage, setCurrentPage] = useState(0);
 
   const StartCourse = Number(currentPage) * Number(pageSize);
@@ -38,21 +25,26 @@ export default function Home() {
   return (
     <main>
       <div className="my-4">
-        {files.slice(StartCourse, EndCourse).map((text) => (
-          <Link key={text.id} href={`/post/${text.id}`}>
-            <div className="border border-transparent w-11/12 mt-3 h-fit mx-auto rounded-xl shadow-custom-shadow">
-              <div className="text-base font-bold mt-4 mx-6">{text.title}</div>
-              <div className="text-sm font-normal mr-6 mt-1 fontcolor1">
-                {text.date}
+        {files
+          .sort((a: any, b: any) => a.id - b.id)
+          .slice(StartCourse, EndCourse)
+          .map((text: any) => (
+            <Link key={text.id} href={`/post/${text.id}`}>
+              <div className="border border-transparent w-11/12 mt-3 h-fit mx-auto rounded-xl shadow-custom-shadow">
+                <div className="text-base font-bold mt-4 mx-6">
+                  {text.title}
+                </div>
+                <div className="text-sm font-normal mr-6 mt-1 fontcolor1">
+                  {text.date}
+                </div>
+                <div className="text-sm font-light text-justify mt-2 mb-4 mx-6 fontcolor1">
+                  {text.text.length > textLength
+                    ? `${text.text.slice(0, textLength)}...`
+                    : text.text}
+                </div>
               </div>
-              <div className="text-sm font-light text-justify mt-2 mb-4 mx-6 fontcolor1">
-                {text.text.length > textLength
-                  ? `${text.text.slice(0, textLength)}...`
-                  : text.text}
-              </div>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          ))}
       </div>
       {files.length > pageSize && (
         <Pagination
